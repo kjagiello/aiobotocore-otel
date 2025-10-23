1b9c6f3bcf83e3122cbb719c40348d2dda7eb9d9
Krzysztof Jagiello <me@kjagiello.com>
2025-10-23 22:38:36 +0200

    Add support for Python 3.14

--- noxfile.py
+++ noxfile.py
@@ -8,8 +8,8 @@ def uv_run(session: nox.Session, *args: str) -> None:
     session.run("uv", "run", "--python", session.python, "--active", *args)
 
 
-@nox.session(python=["3.9", "3.10", "3.11", "3.12", "3.13"])
-@nox.parametrize("aiobotocore", ["2.24.1", "2.24.2", "2.25"])
+@nox.session(python=["3.9", "3.10", "3.11", "3.12", "3.13", "3.14"])
+@nox.parametrize("aiobotocore", ["2.24.2", "2.25"])
 def test(session: nox.Session, aiobotocore: str) -> None:
     uv_run(
         session,
